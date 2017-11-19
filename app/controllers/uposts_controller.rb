4b87d0f49fd39d00d75f8514baa66e74c68f8864
Don kim <dickings@likelion.org>
2017-11-19 13:19:55 +0000

    upostup2

--- app/controllers/uposts_controller.rb
+++ app/controllers/uposts_controller.rb
@@ -4,27 +4,24 @@ before_action :check_ownership, only: [:edit, :update, :destroy]
 
 
   def index
-  if params[:category]
-   @posts = Upost.where(:category => params[:category]).paginate(:page => params[:page]).order('created_at DESC')
-  else
-   @posts = Upost.paginate(:page => params[:page]).order('created_at DESC')
-  end 
-  
-  if params[:search]
-    if params[:selecto]=="1"
-    @posts = Upost.search1(params[:search]).order("created_at DESC").paginate(:page => params[:page])
-    elsif params[:selecto]=="2"
-    @posts = Upost.search2(params[:search]).order("created_at DESC").paginate(:page => params[:page])
-    elsif params[:selecto]=="3"
-    @posts = Upost.search3(params[:search]).order("created_at DESC").paginate(:page => params[:page])
-    elsif params[:selecto]=="4"
-    @users = User.search4(params[:search])
+    if params[:category]
+     @posts = Upost.where(:category => params[:category]).paginate(:page => params[:page]).order('created_at DESC')
+    else
+      if params[:search]
+        if params[:selecto]=="1"
+        @posts = Upost.search1(params[:search]).order("created_at DESC").paginate(:page => params[:page])
+        elsif params[:selecto]=="2"
+        @posts = Upost.search2(params[:search]).order("created_at DESC").paginate(:page => params[:page])
+        elsif params[:selecto]=="3"
+        @posts = Upost.search3(params[:search]).order("created_at DESC").paginate(:page => params[:page])
+        elsif params[:selecto]=="4"
+        @users = User.search4(params[:search])
+        end
+      else
+       @posts = Upost.paginate(:page => params[:page]).order('created_at DESC')
+      end 
     end
-  else
-   @posts = Upost.paginate(:page => params[:page]).order('created_at DESC')
-  end 
   end
-  
   def create
       new_post = Upost.new(user_id: current_user.id, title: params[:title], content: params[:content], image: params[:image], category: params[:category])
       if new_post.save
