f1b543dcd91a5139a8c0993c15a132b299bdf4e9
Don kim <dickings@likelion.org>
2018-02-16 16:14:20 +0000

    commiter2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
       @hbars = Bar.order('score DESC').limit(4)   
       @trposts = Upost.where(:category => 2).order("created_at DESC").limit(10)
       @ntposts = Upost.where(:category => 3).order("created_at DESC").limit(10)
-      @gnposts = Upost.where(:category => 1).joins(:ucomments).order("COUNT(ucomments.id) DESC").limit(10)
+      @gnposts = Upost.where(:category => 1).order("created_at DESC").limit(10)
   end
   
   def sorrybut
